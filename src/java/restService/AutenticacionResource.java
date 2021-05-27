169adf5a1a96d43e7debeab92f985a9559649450
Diana Castro <dianae.castro@gmail.com>
2021-05-26 20:21:34 -0700

    Validar token, obtener datos y actualizar token firebase

--- src/java/restService/AutenticacionResource.java
+++ src/java/restService/AutenticacionResource.java
@@ -4,10 +4,17 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PUT;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import equipo0_dominio.Paciente;
+import equipo0_dominio.Usuario;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import negocios.FactoryNegocios;
+import negocios.INegociosAuthPac;
 
 /**
  * REST Web Service
@@ -17,6 +24,8 @@ import javax.ws.rs.core.MediaType;
 @Path("auth")
 public class AutenticacionResource {
 
+    INegociosAuthPac negocios;
+
     @Context
     private UriInfo context;
 
@@ -24,27 +33,82 @@ public class AutenticacionResource {
      * Creates a new instance of AutenticacionResource
      */
     public AutenticacionResource() {
+        negocios = FactoryNegocios.getFachadaAuthPac();
     }
 
-    /**
-     * Retrieves representation of an instance of restService.AutenticacionResource
-     * @return an instance of java.lang.String
-     */
-    @GET
+    @POST
+    @Path("validartoken")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response postValidarToken(String entrada) {
+        try {
+            return Response.status(200).entity("1").build();
+        } catch (Exception ex) {
+            throw new WebApplicationException(Response.Status.UNAUTHORIZED);
+        }
+    }
+
+    @POST
+    @Path("obtenerdatos")
     @Produces(MediaType.APPLICATION_JSON)
-    public String getJson() {
-        String json="{"
-                + "\"estado\":\"conectado\""
-                + "}";
-        return json;
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response postObtenerDatos(String json) {
+        try {
+            Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
+
+            Usuario usuario = gson.fromJson(json, Usuario.class);
+
+            Paciente paciente = negocios.obtenerDatosPaciente(usuario.getUsername());
+
+            if (paciente != null) {
+                return Response.status(200).entity(gson.toJson(paciente)).build();
+            }
+            return Response.status(404).entity("{0}").build();
+        } catch (Exception e) {
+            return Response.status(404).entity("{0}").build();
+        }
     }
 
-    /**
-     * PUT method for updating or creating an instance of AutenticacionResource
-     * @param content representation for the resource
-     */
-    @PUT
+    @POST
+    @Path("actualizartokenfirebase")
+    @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public void putJson(String content) {
+    public Response postActualizaTokenFirebase(String json) {
+        try {
+            System.out.println(json);
+            Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
+            
+            Paciente usuario = gson.fromJson(json, Paciente.class);
+
+            return Response.status(200).entity(gson.toJson(negocios.actualizarTokenFirebase(usuario.getUsername(), usuario.getTokenFirebase()))).build();
+
+        } catch (Exception e) {
+            return Response.status(404).entity("{0}").build();
+        }
+    }
+
+    @POST
+    @Path("login")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String postLogin(String json) {
+        try {
+            Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
+
+            Usuario usuario = gson.fromJson(json, Usuario.class);
+
+            System.out.println(usuario);
+
+            String token = negocios.iniciarSesion(usuario.getUsername(), usuario.getPassword());
+
+            if (token == null) {
+                throw new WebApplicationException(Response.Status.UNAUTHORIZED);
+            } else {
+                return String.format("{\"token\":\"%s\"}", token);
+            }
+
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            throw new WebApplicationException(Response.Status.UNAUTHORIZED);
+        }
     }
 }
